b1bd75d2cde033a6bf4b5b04a124bd419c15ddab
extrastufff054 <extrastuff054@gmail.com>
2023-04-04 01:02:58 +0530

    Master

---
+++ hello.c
@@ -0,0 +1,11 @@
+//writing a c program to print hello world using github copilot
+
+
+#include<stdio.h>
+int main()
+{
+    //github copilot work you bitch
+    printf("hello world\n");
+    return 0;
+    
+}
